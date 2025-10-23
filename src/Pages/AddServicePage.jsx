134c04482a81a698186802e1d1408eefc34ba48a
iqbalxrr <iamekbal7500@gmail.com>
2025-10-23 23:51:26 +0600

    add imgbb api features

--- src/Pages/AddServicePage.jsx
+++ src/Pages/AddServicePage.jsx
@@ -45,6 +45,7 @@ const AddServicePage = () => {
 
     try {
       setLoading(true);
+      //  Upload to Imgbb API
       const res = await axios.post(
         `https://api.imgbb.com/1/upload?key=${IMGBB_API_KEY}`,
         form
